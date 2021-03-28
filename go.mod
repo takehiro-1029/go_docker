5efffbd8b18008342b446f93d952fd2f9a6b8a37
takehiro-1029 <masamasa.3332@gmail.com>
2021-03-28 20:56:26 +0900

    mysqlの処理追加

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/friendsofgo/errors v0.9.2
 	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/stretchr/testify v1.6.1 // indirect
 	github.com/volatiletech/null/v8 v8.1.2
 	github.com/volatiletech/sqlboiler/v4 v4.5.0
--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"go_docker/dao"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func run() error {
+
+	dsn := mysqlDSN()
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return err
+	}
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
@@ -17,7 +34,7 @@ func run() error {
 		w.Write([]byte("welcome"))
 	})
 	r.Get("/welcome", func(w http.ResponseWriter, r *http.Request) {
-		test()
+		test(w, r, db)
 	})
 	http.ListenAndServe(":8080", r)
 
@@ -30,6 +47,24 @@ func main() {
 	}
 }
 
-func test() {
-	fmt.Println("www")
+func test(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	ctx := r.Context()
+	a, err := dao.Users(
+		dao.UserWhere.ID.EQ("fff"),
+	).One(ctx, db)
+	if err != nil {
+		w.Write([]byte("何もないお"))
+		return
+	}
+	w.Write([]byte(a.FirstName))
+	w.Write([]byte(a.LastName))
+}
+
+// mysqlDSN MySQLの接続に必要なdata source nameを返す
+func mysqlDSN() string {
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbName)
 }
